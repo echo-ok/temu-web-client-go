4e1751253f5d45f11e140bc34fd44eba710409d6
bestk <1138493417@qq.com>
2024-12-18 14:06:40 +0800

    refactor(errors, client): streamline error handling and update authentication method signature
    - Modified the LoginSellerCentralByCode method to return an additional value for improved error handling.
    - Updated the TemuErrorCode structure to remove the Message field and utilize the Err field for error messages, enhancing clarity in error reporting.
    - Adjusted error messages for specific error codes to provide more context and consistency.
    - These changes improve the maintainability and readability of the error handling and authentication processes.

--- normal/temu_errors.go
+++ normal/temu_errors.go
@@ -7,31 +7,22 @@ import (
 
 // TemuErrorCode TEMU错误码枚举
 type TemuErrorCode struct {
-	Code    int
-	Message string
-	Err     error
+	Code int
+	Err  error
 }
 
 var (
-	TemuErrorNeedSMSCode             TemuErrorCode = TemuErrorCode{Code: 6000001, Message: "账号发生变更"}  // 需要短信验证码
-	TemuErrorNeedVerifyCode          TemuErrorCode = TemuErrorCode{Code: 6000002, Message: "需要图形验证码"}  // 需要图形验证码
-	TemuErrorAccountPasswordNotMatch TemuErrorCode = TemuErrorCode{Code: 40002001, Message: "账号密码不匹配"} // 账号密码不匹配
- 
+	TemuErrorNeedSMSCode             TemuErrorCode = TemuErrorCode{Code: 6000001, Err: errors.New("账号发生变更")}   // 需要短信验证码
+	TemuErrorNeedVerifyCode          TemuErrorCode = TemuErrorCode{Code: 6000002, Err: errors.New("需要图形验证码")}  // 需要图形验证码
+	TemuErrorAccountPasswordNotMatch TemuErrorCode = TemuErrorCode{Code: 40002001, Err: errors.New("账号密码不匹配")} // 账号密码不匹配
 )
 
-func (e *TemuErrorCode) Error() string {
-	if e.Message != "" {
-		return fmt.Sprintf("%s: %s", e.Message, e.Err.Error())
-	}
-	return e.Err.Error()
-}
-
-func (e *TemuErrorCode) Unwrap() error {
-	return e.Err
+func (e *TemuErrorCode) Error() error {
+	return fmt.Errorf("%d: %s", e.Code, e.Err.Error())
 }
 
 var ErrNeedSMSCode = errors.New("需要短信验证码")
-var ErrNeedVerifyCode = errors.New("需要图形验证码, 请在TEMU后台手动登录，设置Cookie再试")
+var ErrNeedVerifyCode = errors.New("需要图形验证码，请在TEMU后台手动登录，设置Cookie再试")
 
 func GetErrorByCode(code int, message string) TemuErrorCode {
 	//  遍历所有错误码，找到对应的错误
@@ -41,13 +32,13 @@ func GetErrorByCode(code int, message string) TemuErrorCode {
 		TemuErrorAccountPasswordNotMatch,
 	} {
 		if e.Code == code {
-			e.Message = message
+			e.Err = errors.New(message)
 			return e
 		}
 	}
+
 	return TemuErrorCode{
-		Code:    code,
-		Message: message,
-		Err:     errors.New("unknown error"),
+		Code: code,
+		Err:  errors.New(message),
 	}
 }
